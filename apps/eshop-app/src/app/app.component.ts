360cc61021162d2d96366d2444a0def525ac5608
Alvaro Morales <amorales@integralit.cl>
2022-08-27 15:28:44 -0400

    initial commit

---
+++ apps/eshop-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ecommerce-brand-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'eshop-app';
+}
